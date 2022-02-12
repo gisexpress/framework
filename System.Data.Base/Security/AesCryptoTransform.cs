29a7b57a4cf8882a8b2af935f6d6793be78a7da6
Cem Özkan <96351409+gisexpress@users.noreply.github.com>
2022-02-12 16:47:15 +0300

    Add files via upload

---
+++ System.Data.Base/Security/AesCryptoTransform.cs
@@ -0,0 +1,10 @@
+﻿namespace System.Security.Cryptography
+{
+    internal class AesCryptoTransform : SymmetricCryptoTransform
+    {
+        public AesCryptoTransform(string password)
+            : base(new AesManaged(), password)
+        {
+        }
+    }
+}
---
+++ System.Data.Base/Security/SymmetricCryptoTransform.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+
+namespace System.Security.Cryptography
+{
+    public class SymmetricCryptoTransform : CryptoTransform
+    {
+        protected SymmetricCryptoTransform(SymmetricAlgorithm algorithm, string password)
+            : base(password)
+        {
+            Algorithm = algorithm;
+            Algorithm.Key = SecureKey.GetBytes(Algorithm.KeySize / 8);
+            Algorithm.IV = SecureKey.GetBytes(Algorithm.BlockSize / 8);
+
+            Encryptor = Algorithm.CreateEncryptor();
+            Decryptor = Algorithm.CreateDecryptor();
+        }
+
+        protected readonly SymmetricAlgorithm Algorithm;
+
+        protected readonly ICryptoTransform Encryptor;
+        protected readonly ICryptoTransform Decryptor;
+
+        public override int KeySize
+        {
+            get { return Algorithm.KeySize; }
+        }
+
+        public override bool Reusable
+        {
+            get { return false; }
+        }
+
+        public override Stream CreateEncryptStream(Stream stream)
+        {
+            return new CryptoStream(stream, Encryptor, CryptoStreamMode.Write);
+        }
+
+        public override Stream CreateDecryptStream(Stream stream)
+        {
+            return new CryptoStream(stream, Decryptor, CryptoStreamMode.Read);
+        }
+
+        public override void Dispose()
+        {
+            Encryptor.Dispose();
+            Decryptor.Dispose();
+            Algorithm.DisposeSafely();
+            base.Dispose();
+        }
+    }
+}
